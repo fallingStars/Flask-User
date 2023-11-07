c7dc2b9bc9953ca9dd922b39fb8e7a194708040b
halfmoon <823906197@qq.com>
2023-11-07 16:03:06 +0800

    modify: update dependent packages version to lastest version, and impair the upcoming problems when executing runserver.py

--- example_apps/quickstart_app.py
+++ example_apps/quickstart_app.py
@@ -5,9 +5,7 @@
 # - Using string-based templates (instead of file-based templates)
 
 from flask import Flask, render_template_string
-from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin
-
+from flask_user import login_required
 
 # Class-based application configuration
 class ConfigClass(object):
@@ -33,32 +31,6 @@ def create_app():
     # Create Flask app load app.config
     app = Flask(__name__)
     app.config.from_object(__name__+'.ConfigClass')
-
-    # Initialize Flask-SQLAlchemy
-    db = SQLAlchemy(app)
-
-    # Define the User data-model.
-    # NB: Make sure to add flask_user UserMixin !!!
-    class User(db.Model, UserMixin):
-        __tablename__ = 'users'
-        id = db.Column(db.Integer, primary_key=True)
-        active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
-
-        # User authentication information. The collation='NOCASE' is required
-        # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
-        username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
-        password = db.Column(db.String(255), nullable=False, server_default='')
-        email_confirmed_at = db.Column(db.DateTime())
-
-        # User information
-        first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-        last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-
-    # Create all database tables
-    db.create_all()
-
-    # Setup Flask-User and specify the User data-model
-    user_manager = UserManager(app, db, User)
 
     # The Home page is accessible to anyone
     @app.route('/')
--- runserver.py
+++ runserver.py
@@ -1,6 +1,35 @@
 from example_apps.quickstart_app import create_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import UserManager, UserMixin
 
 app = create_app()
+
+# Initialize Flask-SQLAlchemy
+db = SQLAlchemy(app)
+
+# Define the User data-model.
+# NB: Make sure to add flask_user UserMixin !!!
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+    # User authentication information. The collation='NOCASE' is required
+    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    email_confirmed_at = db.Column(db.DateTime())
+
+    # User information
+    first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+
+# Setup Flask-User and specify the User data-model
+user_manager = UserManager(app, db, User)
+
+with app.app_context():
+    # Create all database tables
+    db.create_all()
 
 app.run(host='0.0.0.0', port=5000, debug=True)
 
